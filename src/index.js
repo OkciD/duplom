96eee951e28ab66f08a9de0fe02d046be3313584
Yury Baykov <y.baykov@corp.mail.ru>
2019-05-27 01:40:34 +0300

    initial vk api calls

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import 'normalize.css';
 import './styles/style.scss';
-import qs from 'qs';
 
-const hashObject = qs.parse(window.location.hash.slice(1));
+const apiVersion = '5.95';
+const appId = '6998698';
 
-if (Object.keys(hashObject).length === 0) {
-	const authQueryString = qs.stringify({
-		client_id: '6998698',
-		redirect_uri: 'http://localhost:8080',
-		response_type: 'token',
-		v: '5.95'
-	});
+VK.init({ apiId: appId });
 
-	window.location.href = `https://oauth.vk.com/authorize?${authQueryString}`;
-}
-
-console.log(hashObject);
-
-const { access_token: accessToken } = hashObject;
-
+VK.Api.call(
+	'users.get',
+	{
+		user_ids: 225300011,
+		v: apiVersion
+	},
+	(r) => { r.response && alert('Привет, ' + r.response[0].first_name) }
+);
