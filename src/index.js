162b11737d94da0b7d91fa34e8b309ed8bdebfa4
Yury Baykov <y.baykov@corp.mail.ru>
2019-06-10 11:49:12 +0300

    rendering your friend's friends

--- src/index.js
+++ src/index.js
@@ -4,44 +4,65 @@ import * as VkApi from './modules/vkApi'
 import { draw } from './modules/graph';
 import queryString from './utils/queryString';
 import getGroupId from './modules/grouper';
+import deepmerge from 'deepmerge';
+
+async function createFriendsGraphData(rootNode = { id: 0, caption: 'Я' }) {
+	const { items: friends } = await VkApi.call(
+		'friends.get',
+		{
+			...(rootNode.id) && { user_id: rootNode.id },
+			order: 'name',
+			fields: 'city,education'
+		}
+	);
+	const groupingField = queryString.get('groupingField');
+
+	return {
+		nodes: [
+			rootNode,
+			...friends.map((friend) => {
+				const { id, first_name, last_name } = friend;
+
+				return {
+					id,
+					caption: `${first_name} ${last_name}`,
+					group: getGroupId(friend, groupingField)
+				}
+			})
+		],
+		links: friends.map(({ id }) => ({
+			source: rootNode.id,
+			target: id,
+			value: 1
+		}))
+	}
+}
 
 VkApi.init();
 
-VkApi.call(
-	'friends.get',
-	{
-		order: 'name',
-		fields: 'city,education'
-	}
-)
-	.then((response) => {
-		const groupingField = queryString.get('groupingField');
-		const friends = response.items;
+createFriendsGraphData()
+	.then((graphData) => {
+		const selectedFriendsIds = queryString.get('friends');
 
-		return {
-			nodes: [
-				{
-					id: 0,
-					caption: 'Я'
-				},
-				...friends.map((friend) => {
-					const { id, first_name, last_name } = friend;
-
-					return {
-						id,
-						caption: `${first_name} ${last_name}`,
-						group: getGroupId(friend, groupingField)
-					}
-				})
-			],
-			links: friends.map(({ id }) => ({
-				source: 0,
-				target: id,
-				value: 1
-			}))
+		// TODO: maybe this condition can be reduced to !Array.isArray
+		if (!selectedFriendsIds || !Array.isArray(selectedFriendsIds)) {
+			return graphData;
 		}
+
+		// TODO: eto pizdec, this needs to be refactored
+		return selectedFriendsIds.reduce( // OMG, real promise sequence!
+			(accumulatorPromise, selectedFriendId) => accumulatorPromise.then(async () =>
+				deepmerge(
+					graphData,
+					await createFriendsGraphData(
+						graphData.nodes.find(({ id }) => (+id === +selectedFriendId))
+					)
+				)
+			),
+			Promise.resolve()
+		);
 	})
-	.then(async (graphData) => { // OMG, real promise sequence!
+	.then(async (graphData) => {
 		await graphData.nodes.reduce(
 			(accumulatorPromise, { id: friendId }) => accumulatorPromise.then(() =>
 				VkApi.call('friends.getMutual', { target_uid: friendId })
